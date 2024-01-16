99fbc0b6c8c6f3ccf15541c782d7c8832d00e7b9
Brigitte Boerland <brigitteboerland@hotmail.com>
2024-01-16 15:52:16 +0100

    Functional search bar

--- components/DrinkSearch.jsx
+++ components/DrinkSearch.jsx
@@ -5,14 +5,20 @@ import { DrinkList } from "./DrinkList";
 import { availableDrinks } from '../utils/data';
 
 export const DrinkSearch = ({ clickFn }) => {
-    const [searchField, setSearchField] = useState('test drink');
+    const [searchField, setSearchField] = useState('');
+    const handleChange = (event) => {
+        setSearchField(event.target.value);
+    };
+    const matchedDrinks = availableDrinks.filter((drink) => {
+        return drink.name.toLowerCase().includes(searchField.toLowerCase());
+    });
 
     return (
         <>
             <label>Search for drinks:</label>
-            <TextInput />
+            <TextInput changeFn={handleChange} />
             <p>{searchField}</p>
-            <DrinkList clickFn={clickFn} drinks={availableDrinks} />
+            <DrinkList clickFn={clickFn} drinks={matchedDrinks} />
         </>
     )
 }
--- components/UI/TextInput.jsx
+++ components/UI/TextInput.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable */
+
 import './TextInput.css'
-export const TextInput = () => {
+export const TextInput = ({ changeFn }) => {
     return (
 
-        <input className="text-input"></input>
+        <input className="text-input" onChange={changeFn}></input>
 
     )
 }
